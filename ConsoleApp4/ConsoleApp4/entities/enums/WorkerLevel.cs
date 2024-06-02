52112d2015500165e75a741de0e68936248b968b
Rafael Nascimento <rn206041@gmail.com>
2024-06-01 22:09:36 -0300

    Projeto Worker

---
+++ ConsoleApp4/ConsoleApp4/entities/enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.entities.enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 1,
+        Pleno = 2,
+        Senior = 3
+    }
+}
